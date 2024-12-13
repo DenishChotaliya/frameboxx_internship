78ea53bc18e4b9472c02b2ea1fd0c4954ce80b38
Denish Chotaliya <128218250+DenishChotaliya@users.noreply.github.com>
2024-12-13 12:17:40 +0530

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,26 @@
+$(".slick-carousel").slick({
+    speed: 5000,
+		autoplay: true,
+		autoplaySpeed: 0,
+		cssEase: 'linear',
+    slidesToShow: 5,
+  	slidesToScroll: 1,
+    infinite: true,
+    swipeToSlide: true,
+  	centerMode: true,
+    focusOnSelect: true,
+    responsive: [
+            {
+              breakpoint: 750,
+              settings: {
+                slidesToShow: 3,
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 2,
+              }
+            }
+            ]
+});
